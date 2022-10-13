cb85597c166c7012dd471a724c67bdecdaeede52
Luiz Calaça <luizcalaca@gmail.com>
2022-10-13 08:44:08 -0300

    feat: Javascript

--- tree.js
+++ tree.js
@@ -5,12 +5,29 @@ class BinaryTree { //O(log n)
         this.left = null
         this.right = null
     }
+
+    static addAll(root) {
+        if(root == null) 
+            return 0
+        return (root.value + this.addAll(root.left) + this.addAll(root.right))
+    }
 }
 
+let root = new BinaryTree(1);
+root.left = new BinaryTree(2);
+root.right = new BinaryTree(3);
+root.left.left = new BinaryTree(4);
+root.left.right = new BinaryTree(5);
+root.right.left = new BinaryTree(6);
+root.right.right = new BinaryTree(7);
+root.right.left.right = new BinaryTree(8);
+
+let sum = BinaryTree.addAll(root);
+console.log(sum)
 //Level 0: 2^0 = 1
 //Level 1: 2^1 = 2
 //Level 2: 2^2 = 4
 //Level 3: 2^3 = 8
 
-//total of nodes = 2^h - 1 (h is the level's quantity)
+//total of BinaryTrees = 2^h - 1 (h is the level's quantity)
 
