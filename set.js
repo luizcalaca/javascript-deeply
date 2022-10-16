6b04c131cf76ba48b728d29fa8f9b6bc20f16b82
Luiz Calaça <luizcalaca@gmail.com>
2022-10-15 22:39:15 -0300

    feat: Javascript

--- set.js
+++ set.js
@@ -2,12 +2,23 @@ const setA = new Set();
 setA.add(1);
 setA.add(2);
 setA.add(3);
-
 const setB = new Set();
 setB.add(3);
 setB.add(4);
 setB.add(5);
 setB.add(6);
 
-const unionAB = new Set([...setA, ...setB])
-console.log(unionAB.values());
+//const unionAB = new Set([...setA, ...setB])
+//console.log(unionAB.values());
+
+    const subSet = (setB, setA) => {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            if(!setA.has(element))
+                return false
+        }
+    return true
+    }
+
+console.log(subSet(setB, setA))
+
---
+++ two_string_substring.js
@@ -0,0 +1,38 @@
+
+const verifySubString = 
+(string_first, string_second) => 
+{
+    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+   
+    let indexes = []
+    for (let index = 0; index < string_first.length; index++) {
+        if(alphabet.includes(string_first[index])) {
+            indexes.push(alphabet.indexOf(string_first[index]))
+        }
+    }
+
+    let indexes2 = []
+    for (let index2 = 0; index2 < string_second.length; index2++) {
+        if(alphabet.includes(string_second[index2])) {
+            indexes2.push(alphabet.indexOf(string_second[index2]))
+        }
+    }
+
+    if((indexes.length > indexes2.length) || (indexes.length === indexes2.length)) {
+        for (let index = 0; index < indexes2.length; index++) {
+            if(indexes2.includes(indexes[index])){
+                return "YES"
+            }
+        }
+        return "NO"
+    }else{
+        for (let index = 0; index < indexes.length; index++) {
+            if(indexes.includes(indexes2[index])){
+                return "YES"
+            }  
+        }
+        return "NO"
+    }
+}
+
+console.log(verifySubString("oooa", "dfga"))
