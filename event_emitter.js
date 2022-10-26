b5222c90060c471867627515391e2bf993f7b05e
Luiz Calaça <luizcalaca@gmail.com>
2022-10-26 11:12:10 -0300

    feat: Javascript

--- event_emitter.js
+++ event_emitter.js
@@ -6,10 +6,17 @@ emitter.on('saved',  (arg) => {
     console.log(`A saved event occurred: name: ${arg.name}, id: ${arg.id}`);
 });
 
+emitter.on('deleted',  (arg) => {
+    console.log(`A deleted event occurred: name: ${arg.name}`);
+});
+
 emitter.emit('saved', {
     id: 1,
     name: 'John Doe'
 });
 
+emitter.emit('deleted', {
+    name: 'Calaca'
+});
 
 
