9a8b963784a788555c316b6e0b3add10f21b9be2
Luiz Calaça <luizcalaca@gmail.com>
2023-06-12 13:27:14 -0300

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -3,11 +3,14 @@ function twoSum(array, target) {
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
         if(array.includes(Math.abs(target-element))){
-            return [index, array.indexOf(Math.abs(target-element))]
+            if(Math.abs(target-element) === array[index])
+                return [index, index+1]
+            else
+                return [index, array.indexOf(Math.abs(target-element))]
         }
     }
 }
 
-const array = [1,6,2,8]
-const target = 10
+const array = [1,1,8,8]
+const target = 2
 console.log(twoSum(array, target))
