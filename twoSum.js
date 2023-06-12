e8c42fdbb141eb20b8f3710afb77494e812fa2db
Luiz Calaça <luizcalaca@gmail.com>
2023-06-12 13:14:52 -0300

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,15 +1,13 @@
 
-const sum = (nums, target) => {
-    const previousValues = {}
-
-    nums.forEach((_element,id) => {
-        const currentNumber = nums[id]
-        const neededValue = target - currentNumber
-        const index2 = previousValues[neededValue]
-        if(index2 != null) {
-            return [index2, id]
-        }else {
-            previousValues[currentNumber] = i
+function twoSum(array, target) {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(array.includes(Math.abs(target-element))){
+            return [index, array.indexOf(Math.abs(target-element))]
         }
-    });
+    }
 }
+
+const array = [1,6,2,8]
+const target = 10
+console.log(twoSum(array, target))
